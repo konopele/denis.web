02a94ccae8df9c2a591c9b712d8fa17ba288b2ab
Nikita <nikita.konopele@gmail.com>
2026-06-04 16:20:40 +0200

    Add Google Analytics 4 tracking script to components.js

--- components.js
+++ components.js
@@ -1,3 +1,16 @@
+// Google Analytics 4
+(function() {
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-XBMERL14JE';
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-XBMERL14JE');
+})();
+
 function initPage(active) {
 
   // NAV — "Услуги" stays active for all service sub-pages too
